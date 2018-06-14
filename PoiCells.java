04cd4bbeae5e2d007c1f09b04adcfda33e8f8c23
huqun <987812492@qq.com>
2018-06-14 17:20:45 +0800

    更新
    20180614更新

--- PoiCells.java
+++ PoiCells.java
@@ -1,16 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+
+
+
 public class PoiCells {
     public final static String regex = "\\$\\{[^\\}]+\\}";
     private Sheet sheet ;
     private Cell cell ;
     private int sheetIndex;
-    
+
     public PoiCells(){}
-    
+
     public PoiCells(Sheet sheet,Cell cell){
         this.sheet = sheet;
         this.cell = cell;
     }
-    
+
     public Sheet getSheet() {
         return sheet;
     }
@@ -19,3 +29,35 @@ public class PoiCells {
     }
     public Cell getCell() {
         return cell;
+    }
+    public void setCell(Cell cell) {
+        this.cell = cell;
+    }
+    public int getSheetIndex() {
+        return sheetIndex;
+    }
+    public void setSheetIndex(int sheetIndex) {
+        this.sheetIndex = sheetIndex;
+    }
+
+    /**
+     * ��ȡ��ǰ�е��С����ʽ������
+     * @return
+     */
+    public List getExps(){
+        List list = new ArrayList();
+        if(this.cell!=null){
+            String contents = this.cell.getStringCellValue();
+            if(!"".equals(contents)){
+                list = StringUtil.search(this.regex, contents);
+            }
+        }
+        return list;
+    }
+
+    public String getFomatContext(){
+        String contents = this.cell.getStringCellValue();
+        //while(contents.)
+        return contents;
+    }
+}
